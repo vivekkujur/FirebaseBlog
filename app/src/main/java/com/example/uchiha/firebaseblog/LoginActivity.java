727ec9c58303039b51984a3fd73a393d915cf7bc
uchiha <vivekkujur.jnv@gmail.com>
2018-08-29 01:09:28 +0530

    pre setup page

---
+++ app/src/main/java/com/example/uchiha/firebaseblog/LoginActivity.java
@@ -0,0 +1,113 @@
+package com.example.uchiha.firebaseblog;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.LoginFilter;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button btn_login, btn_reg;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth=FirebaseAuth.getInstance();
+        database= FirebaseDatabase.getInstance().getReference().child("User");
+        database.keepSynced(true);
+
+
+        email=findViewById(R.id.loginemailfield);
+        password=findViewById(R.id.loginpasswordfield);
+        btn_login=findViewById(R.id.login_Btn);
+        btn_reg=findViewById(R.id.login_not);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startlogin();
+            }
+        });
+    }
+
+    private void startlogin() {
+
+        String Email=email.getText().toString().trim();
+        String Password= password.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(Email) && !TextUtils.isEmpty(Password)){
+
+            mAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if(task.isSuccessful()){
+
+                        checkUserExist();
+
+                    }else {
+                        Toast.makeText(LoginActivity.this,"error login",Toast.LENGTH_LONG).show();
+                    }
+
+
+                }
+            });
+
+        }
+
+
+    }
+
+    private void checkUserExist() {
+
+        final String user_id=mAuth.getCurrentUser().getUid();
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.hasChild(user_id)){
+
+                    Intent main_intent= new Intent(LoginActivity.this,MainActivity.class) ;
+                    main_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(main_intent);
+
+
+                }else{
+
+                    Intent setup_intent= new Intent(LoginActivity.this,MainActivity.class) ;
+                    setup_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(setup_intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/uchiha/firebaseblog/MainActivity.java
+++ app/src/main/java/com/example/uchiha/firebaseblog/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,16 +20,19 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private  DatabaseReference mref;
-   FirebaseDatabase firebaseDatabase;
+    private DatabaseReference mdatabaseuser;
     private FirebaseAuth mAuth;
     private  FirebaseAuth.AuthStateListener authStateListener;
 
@@ -37,14 +41,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseDatabase=FirebaseDatabase.getInstance();
         mref=FirebaseDatabase.getInstance().getReference().child("Blog");
+        mdatabaseuser=FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mref.keepSynced(true);
+        mdatabaseuser.keepSynced(true);
+
         mAuth = FirebaseAuth.getInstance();
+
         authStateListener= new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser()== null){
-                    Intent  login_intent= new Intent(MainActivity.this,RegisterActivity.class) ;
+                    Intent  login_intent= new Intent(MainActivity.this,LoginActivity.class) ;
                     login_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(login_intent);
 
@@ -62,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        checkUserExist();
 
       mAuth.addAuthStateListener(authStateListener);
         FirebaseRecyclerOptions<Blog> options =
@@ -95,6 +106,32 @@ public class MainActivity extends AppCompatActivity {
 //       recyclerView.setAdapter(firebaseRecyclerAdapter);
         firebaseRecyclerAdapter.startListening();
         recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+
+    private void checkUserExist() {
+
+        final String user_id=mAuth.getCurrentUser().getUid();
+        mdatabaseuser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(!dataSnapshot.hasChild(user_id)){
+
+                    Intent setup_inten= new Intent(MainActivity.this,SetupActivity.class) ;
+                    setup_inten.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(setup_inten);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
 
